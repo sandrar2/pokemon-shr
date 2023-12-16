8963d6b1829ec29c25bcc5a5211cc301a9ab3afd
sandrar2 <sandramonicahuamanrondon.4041@gmail.com>
2023-12-16 11:48:45 -0500

    Mi primera vista

---
+++ owner/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('owner_list/', views.owner_list, name='owner_list')
+]
+
+
--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def owner_list(request):
+
+    data_context = {
+        'nombre': 'Katty Paredes',
+        'edad': 20,
+        'dni': '12345678',
+        'pais': 'Peru',
+        'vigente': False,
+    }
+
+    return render(request,'owner_list.html', context={'data': data_context})
+
+
+
